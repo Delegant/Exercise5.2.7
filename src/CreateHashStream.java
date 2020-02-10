0458958debe12532087ab0cb96d2cb8cec3c4805
Delegant <59837698+Delegant@users.noreply.github.com>
2020-02-10 16:32:44 +0300

    added test

---
+++ src/CreateHashStream.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class CreateHashStream {
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        //past your code and dell "return 0"
+        return 0;
+    }
+}
